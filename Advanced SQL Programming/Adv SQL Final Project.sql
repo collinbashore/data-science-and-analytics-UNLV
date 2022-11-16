b3e94502c0c161601edcb225093170098c1408cf
Collin Bashore <79961362+collinbashore@users.noreply.github.com>
2022-11-16 12:44:46 -0800

    Update Adv SQL Final Project.sql

--- Advanced SQL Programming/Adv SQL Final Project.sql
+++ Advanced SQL Programming/Adv SQL Final Project.sql
@@ -1,36 +1,13 @@
-SELECT DISTINCT name
-FROM sakila.category;
+-- This stored procedure allows the user to input the first name and return the list of all the actors that share that same first name.
 
 DELIMITER $$
 CREATE PROCEDURE sp_actor_first_name(IN actor_firstname VARCHAR(45))
 BEGIN
 	SELECT first_name, CONCAT(first_name, ' ' , last_name) AS actor
 	FROM actor
-    WHERE first_name = actor_firstname;
+        WHERE first_name = actor_firstname;
 END $$
 
 CALL sp_actor_first_name('Penelope');
 
 CALL sp_actor_first_name('Collin');
-
-Delimiter //
-create procedure ActorName(IN Name VARCHAR(255))
-Begin
-Select *
-from actor
-Where first_name = Name;
-END
-//
-
-CALL ActorName(‘Penelope’);
-
-SELECT *
-FROM film;
-
-SELECT * 
-FROM film_category;
-
-SELECT *
-FROM category;
-
-
